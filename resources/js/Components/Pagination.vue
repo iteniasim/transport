7b4360587583ea10cca876cee8f7e89d1109c118
iteniasim <iteniasim.hulkstar@gmail.com>
2024-12-05 12:28:25 +0545

    update ui for pagination component

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -10,18 +10,25 @@ defineProps({
 </script>
 
 <template>
-    <nav class="flex items-center justify-center gap-x-1">
+    <nav class="flex items-center justify-center gap-2">
         <template v-for="(link, index) in links" :key="index">
             <!-- Render a disabled link if the URL is null -->
-            <div v-if="link.url === null" class="btn btn-soft text-gray-400 cursor-not-allowed" v-html="link.label"></div>
+            <div v-if="link.url === null" 
+                 class="inline-flex items-center justify-center h-9 px-4 text-sm font-medium rounded-md bg-gray-100 text-gray-400 cursor-not-allowed">
+                <span v-html="link.label"></span>
+            </div>
 
             <!-- Render active/inactive links -->
-            <Link v-else :href="link.url" :class="[
-                'btn btn-soft',
-                link.active ? 'aria-[current=\'page\']:text-bg-soft-primary' : '',
-                link.label.includes('Previous') || link.label.includes('Next') ? '' : 'btn-square'
-            ]" preserve-scroll :aria-current="link.active ? 'page' : null">
-            <span v-html="link.label"></span>
+            <Link v-else 
+                  :href="link.url" 
+                  :class="[
+                      'inline-flex items-center justify-center h-9 px-4 text-sm font-medium rounded-md transition duration-200 ease-out',
+                      link.active ? 'bg-indigo-400 text-white hover:bg-indigo-500' : 'bg-gray-200 text-gray-700 hover:bg-gray-300',
+                      link.label.includes('Previous') || link.label.includes('Next') ? '' : 'w-9 h-9 p-0'
+                  ]" 
+                  preserve-scroll 
+                  :aria-current="link.active ? 'page' : null">
+                <span v-html="link.label"></span>
             </Link>
         </template>
     </nav>
